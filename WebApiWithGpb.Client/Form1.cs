ab75f1a8f5b15d3a3f77ac42dbf8dd188e232cf2
bertt <btemme@hotmail.com>
2013-05-11 13:38:06 +0200

    running

--- WebApiWithGpb.Client/Form1.cs
+++ WebApiWithGpb.Client/Form1.cs
@@ -1,13 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WebApiWithGpb.Client
@@ -25,6 +18,8 @@ namespace WebApiWithGpb.Client
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/x-protobuf"));
             var result = await client.GetStringAsync("http://localhost:53833/api/persons");
             
+
+
             // try to deserialize
             MessageBox.Show(result);
         }
