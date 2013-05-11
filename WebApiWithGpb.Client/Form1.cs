361528d592f113f5b3cbcaea6ed6a7accbbdcf91
bertt <btemme@hotmail.com>
2013-05-11 11:58:18 +0200

    added client

---
+++ WebApiWithGpb.Client/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebApiWithGpb.Client
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/x-protobuf"));
+            var result = await client.GetStringAsync("http://localhost:53833/api/persons");
+            
+            // try to deserialize
+            MessageBox.Show(result);
+        }
+    }
+}
